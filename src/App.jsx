b5d145f1f5eea483a904fe054a1e21784fc4e074
Seyi Oyemade <seyioyemade918@gmail.com>
2023-05-15 20:31:03 +0100

    Create directory for missions slice

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import {
   BrowserRouter, Routes, Route,
 } from 'react-router-dom';
 import Rockets from './components/rockets/Rockets';
-import Missions from './components/pages/Missions';
+import Missions from './components/missions/Missions';
 import MyProfile from './components/MyProfile';
 import './styles/index.css';
 
