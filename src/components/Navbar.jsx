86ea9e61e1d8e03c1f0f26897a4b4eee56a6aede
Abbas Sarwar <abbassarwar100@gmail.com>
2023-05-15 18:37:01 +0500

    Setup arranged

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import planet from '../assets/planet.png';
 
 const Navbar = () => (
@@ -11,13 +11,13 @@ const Navbar = () => (
     <nav>
       <ul>
         <li>
-          <Link to="/">Rockets</Link>
+          <NavLink to="/">Rockets</NavLink>
         </li>
         <li>
-          <Link to="/missions">Missions</Link>
+          <NavLink to="/missions">Missions</NavLink>
         </li>
         <li>
-          <Link to="/my-profile">My profile</Link>
+          <NavLink to="/my-profile">My profile</NavLink>
         </li>
       </ul>
     </nav>
